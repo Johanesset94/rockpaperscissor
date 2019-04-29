83b55ac4052a551b6328ef0fa8b1b7d5b1de3e6c
Johan Sjöstrand <jsjostrand.esset@gmail.com>
2019-04-29 09:06:12 +0200

    Finished tests, updated README

--- README.md
+++ README.md
@@ -16,3 +16,18 @@ npm run start
 npm test
 ### Lint
 npm run lint
+
+## How to play
+I have been using insomnia (https://insomnia.rest/) to play, but other http clients will work just as fine.
+
+1. Start the server (by running "npm build && npm run start" for example). Make sure you have the IP of the server if playing remotely. In the examples localhost have been used.
+2. Player 1 creates a new game by sending "POST /api/games" with {name: [player1Name]} as body to the ip of server with port 8080 by default. An id is returned. 
+![Alt text](figures/createGame.png "Creating a game with insomnia");
+3. Player 1 sends the id to player 2.
+4. Player 2 sends "POST /api/games/:id/join" with {name: [player2Name]} as body using the id received from player 1, to join the game.
+![Alt text](figures/joinGame.png "Joining a game with insomnia");
+The game is now ongoing, meaning the players can make their moves.
+5. Both players send their moves with "POST /api/games/:id/move" with {name: [playerName], move: ["rock"/"paper"/"scissor"]} as body, using the game id. 
+![Alt text](figures/makeMove.png "Make a move with insomnia");
+6. The game result is obtained by sending "GET /api/games/:id". The players can now see who won.
+![Alt text](figures/getGameStatus.png "Get game status with insomnia");
--- test/apiTest.js
+++ test/apiTest.js
@@ -75,7 +75,7 @@ describe('Running join game tests', () => {
             });
     });
     it('should not allow a player to join a game if no name is provided, returning 400', () => {
-        let req = { };
+        let req = {};
         chai.request(app)
             .post('/api/games/' + gameId + '/join')
             .send(req)
@@ -119,7 +119,7 @@ describe('Running join game tests', () => {
 describe('Running make move tests', () => {
     let gameId;
     const player1 = 'player1', player2 = 'player2';
-    before('Creating a new game for join game tests', () => {
+    before('Creating a new game for make move tests', () => {
         return new Promise(resolve => {
             let req = { name: player1 };
             chai.request(app)
@@ -140,7 +140,7 @@ describe('Running make move tests', () => {
         });
     });
     it('should not allow a player to make a move in a game that does not exists, returning 404', () => {
-        let req = { 
+        let req = {
             name: player1,
             move: 'Rock'
         };
@@ -215,6 +215,72 @@ describe('Running make move tests', () => {
             .end((err, res) => {
                 chai.expect(err).to.be.null;
                 chai.expect(res).to.have.status(200);
+            });
+    });
+});
+// ==== Get game status tests
+// All API calls except create a new game will also return the game status in the body.
+// These tests make sure that the correct game status is returned.
+describe('Running game status tests', () => {
+    let gameId;
+    const player1 = 'player1', player2 = 'player2';
+    before('Creating a new game for game status tests', () => {
+        return new Promise(resolve => {
+            let req = { name: player1 };
+            chai.request(app)
+                .post('/api/games')
+                .send(req)
+                .end((err, res) => {
+                    gameId = res.text;
+                    console.log('Game is setup for game status test with gameId %s', gameId);
+                    resolve();
+                });
+        });
+    });
+    it('should return game status of "created" before player 2 joins', () => {
+        chai.request(app)
+            .get('/api/games/' + gameId)
+            .send()
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res.body.status).to.equal('created');
+                chai.expect(res.body.player1).to.equal(player1);
+            });
+    });
+    it('should return game status "ongoing" after both players joined', () => {
+        let req = { name: player2 };
+        chai.request(app)
+            .post('/api/games/' + gameId + '/join')
+            .send(req)
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res.body.status).to.equal('ongoing');
+                chai.expect(res.body.player2).to.equal(player2);
+            });
+    });
+    it('should return game status "ongoing" still after one player made their move, without revealing what move was made', () => {
+        let req = { name: player1, move: 'rock' };
+        chai.request(app)
+            .post('/api/games/' + gameId + '/move')
+            .send(req)
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res.body.status).to.equal('ongoing');
+                chai.expect(res.body['player1Move']).to.be.undefined;
+                chai.expect(res.body['player2Move']).to.be.undefined;
+            });
+    });
+    it('should return game status "finished" after both players made their move, and reveal everything of the game, also determining the correct winner', () => {
+        let req = { name: player2, move: 'scissor' };
+        chai.request(app)
+            .post('/api/games/' + gameId + '/move')
+            .send(req)
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res.body.status).to.equal('finished');
+                chai.expect(res.body['player1Move']).to.equal('rock');
+                chai.expect(res.body['player2Move']).to.equal('scissor');
+                chai.expect(res.body.result).to.equal(player1 + ' won');
             });
     });
 });
